4e8c79456a82193b9381660c5dff47ba6371efe1
lei hehl <lei.hehl@shopee.com>
2022-06-28 09:06:25 +0800

    fix: 1

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -6,8 +6,8 @@ const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflo
 const mergeRe = /^(Merge pull request|Merge branch)/
 if (!commitRE.test(msg)) {
   if(!mergeRe.test(msg)){
-    console.log('git commit信息校验不通过')
-    console.error(`git commit的信息格式不对, 需要使用 title(scope): desc的格式
+    console.log('git commit：信息校验不通过')
+    console.error(`git commit：信息格式不对, 需要使用 title(scope): desc的格式
       比如 fix: xxbug
       feat(test): add new 
       具体校验逻辑看 scripts/verifyCommit.js
@@ -15,8 +15,8 @@ if (!commitRE.test(msg)) {
     process.exit(1)
   } else {
     if (msg.length < 10) {
-      console.log('git commit信息校验不通过')
-      console.error(`git commit 描述不能少于10个字符`)
+      console.log('git commit：信息校验不通过')
+      console.error(`git commit：描述不能少于10个字符`)
       process.exit(1)
     }
   }
